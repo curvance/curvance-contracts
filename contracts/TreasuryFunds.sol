1e99cb420d5279b88f5c612e7f4fed7edacda14d
yuriarthf <yuri.fernandes@illuvium.io>
2022-01-21 11:11:38 -0300

    feat(backend): adding some soldocs

--- contracts/TreasuryFunds.sol
+++ contracts/TreasuryFunds.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-//receive treasury funds. operator can withdraw
-//allow execute so that certain funds could be staked etc
-//allow treasury ownership to be transfered during the vesting stage
+/**
+ * @title Treasury Funds
+ *
+ * @author Convex Finance, Curvance
+ *
+ * @notice Receive treasury funds, treasurer can withdraw and execute arbitrary external code
+ *
+ * @dev treasuryAdmin can assign treasurer roles, which are responsible for managing treasury's funds.
+ *
+ * @dev Multisig can be implemented by assigning an admin address, so it can add other addresses as treasurers.
+ */
 contract TreasuryFunds {
     using SafeERC20 for IERC20;
     using Address for address;
@@ -16,35 +23,53 @@ contract TreasuryFunds {
     address public treasuryAdmin;
     mapping(address => bool) public isTreasurer;
 
-    /// @dev emit when
+    /// @dev Emit when funds are withdrawn
     event WithdrawTo(IERC20 indexed asset, address indexed to, address indexed receiver, uint256 amount);
 
+    /// @dev Emit when the admin role is changed
     event changedAdmin(address indexed from, address indexed to);
 
+    /// @dev Emit when a new treasurer is nominated
     event newTreasurer(address indexed admin, address indexed nominee);
 
+    /// @dev Emit when a treasurer is removed from the role
     event removeTreasurer(address indexed admin, address indexed treasurer);
 
+    /// @dev Emit when a treasurer resigns
     event resignTreasurer(address indexed resignee);
 
+    /// @dev Emit when making external calls
     event externalCall(address indexed treasurer, address indexed to, uint256 amount);
 
+    /// @dev Only admins can execute functions with this modifier
     modifier adminOnly() {
         require(msg.sender == treasuryAdmin, "!treasuryAdmin");
         _;
     }
 
+    /// @dev Only treasurers can execute functions with this modifier
     modifier treasurerOnly() {
         require(isTreasurer[msg.sender], "!treasurer");
         _;
     }
 
-    constructor() {
-        treasuryAdmin = msg.sender;
-        isTreasurer[msg.sender] = true;
+    /// @dev Zero address is disallowed
+    modifier notZeroAddress(address _addr) {
+        require(_addr != address(0), "Zero address");
+        _;
     }
 
-    function resignAdminRoleTo(address _to) external adminOnly {
+    /**
+     * @dev Initialize contract
+     *
+     * @param _admin treasuryAdmin to be nominated
+     */
+    constructor(address _admin) notZeroAddress(_admin) {
+        treasuryAdmin = _admin;
+        isTreasurer[_admin] = true;
+    }
+
+    function resignAdminRoleTo(address _to) external adminOnly notZeroAddress(_to) {
         isTreasurer[msg.sender] = false;
 
         treasuryAdmin = _to;
@@ -53,7 +78,7 @@ contract TreasuryFunds {
         emit changedAdmin(msg.sender, _to);
     }
 
-    function nominateTreasurer(address _nominee) external adminOnly {
+    function nominateTreasurer(address _nominee) external adminOnly notZeroAddress(_nominee) {
         require(!isTreasurer[_nominee], "Already nominated");
         isTreasurer[_nominee] = true;
 
@@ -67,13 +92,19 @@ contract TreasuryFunds {
         emit removeTreasurer(msg.sender, _treasurer);
     }
 
-    function resignTreasury() external {
-        require(isTreasurer[msg.sender], "!treasurer");
+    function resignTreasury() external treasurerOnly {
         isTreasurer[msg.sender] = false;
 
-        resignTreasurer(msg.sender);
+        emit resignTreasurer(msg.sender);
     }
 
+    /**
+     * @dev Withdraw an amount of a given asset to a given address
+     *
+     * @param _asset The ERC20 compatible asset to be used
+     * @param _amount Amount to be withdrawn
+     * @param _to Address to send the funds
+     */
     function withdrawTo(
         IERC20 _asset,
         uint256 _amount,
@@ -84,6 +115,13 @@ contract TreasuryFunds {
         emit WithdrawTo(_asset, msg.sender, _to, _amount);
     }
 
+    /**
+     * @dev Execute a low-level call to an address
+     *
+     * @param _to Address to execute the call
+     * @param _value The amount of ethers to send
+     * @param _data Call data
+     */
     function execute(
         address _to,
         uint256 _value,
@@ -92,14 +130,5 @@ contract TreasuryFunds {
         _to.functionCallWithValue(_data, _value);
 
         emit externalCall(msg.sender, _to, _value);
-    }
-
-    function stake(
-        IERC20 _asset,
-        address _staker,
-        uint256 _amount
-    ) external treasurerOnly {
-        require(_asset.balanceOf(this) >= _amount, "!funds");
-        // ...
     }
 }
