05230c53ab88cb7048a5e7e5dc80ba8edc851153
zeuscurvance <zeus@curvance.com>
2023-12-19 20:09:43 -0500

    consolidate deploy scripts, add ctoken config update script

--- contracts/market/lendtroller/Lendtroller.sol
+++ contracts/market/lendtroller/Lendtroller.sol
@@ -39,7 +39,7 @@ contract Lendtroller is LiquidityManager, ERC165 {
     uint256 internal constant _TOKEN_NOT_LISTED_SELECTOR = 0xf3e41c92;
     /// `bytes4(keccak256(bytes("Lendtroller__Paused()")))`
     uint256 internal constant _PAUSED_SELECTOR = 0xe192eaaf;
-    
+
     /// STORAGE ///
 
     /// @notice A list of all tokens inside this market for the frontend.
@@ -666,7 +666,7 @@ contract Lendtroller is LiquidityManager, ERC165 {
                 }
             }
         }
-        
+
         tokensListed.push(mToken);
         emit TokenListed(mToken);
     }
