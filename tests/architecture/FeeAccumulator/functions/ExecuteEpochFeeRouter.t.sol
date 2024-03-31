617c84dbca0e1994033fca79ff94c7af52c5d2a5
0xMai <chris@curvance.com>
2024-03-31 16:03:41 -0400

    Comment out fee accumulator crosschain tests since we are moving everything to PMH

--- tests/architecture/FeeAccumulator/functions/ExecuteEpochFeeRouter.t.sol
+++ tests/architecture/FeeAccumulator/functions/ExecuteEpochFeeRouter.t.sol
@@ -1,143 +1,143 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity 0.8.17;
 
-import { TestBaseFeeAccumulator } from "../TestBaseFeeAccumulator.sol";
-import { EpochRolloverData } from "contracts/interfaces/IFeeAccumulator.sol";
-import { FeeAccumulator } from "contracts/architecture/FeeAccumulator.sol";
-import { RewardsData } from "contracts/interfaces/ICVELocker.sol";
+// import { TestBaseFeeAccumulator } from "../TestBaseFeeAccumulator.sol";
+// import { EpochRolloverData } from "contracts/interfaces/IFeeAccumulator.sol";
+// import { FeeAccumulator } from "contracts/architecture/FeeAccumulator.sol";
+// import { RewardsData } from "contracts/interfaces/ICVELocker.sol";
 
-contract ExecuteEpochFeeRouterTest is TestBaseFeeAccumulator {
-    EpochRolloverData data =
-        EpochRolloverData({
-            chainId: 42161,
-            value: _ONE,
-            numChainData: 1,
-            epoch: 0
-        });
+// contract ExecuteEpochFeeRouterTest is TestBaseFeeAccumulator {
+//     EpochRolloverData data =
+//         EpochRolloverData({
+//             chainId: 42161,
+//             value: _ONE,
+//             numChainData: 1,
+//             epoch: 0
+//         });
 
-    function setUp() public override {
-        super.setUp();
+//     function setUp() public override {
+//         super.setUp();
 
-        vm.prank(centralRegistry.feeAccumulator());
-        cveLocker.recordEpochRewards(_ONE);
+//         vm.prank(centralRegistry.feeAccumulator());
+//         cveLocker.recordEpochRewards(_ONE);
 
-        skip(veCVE.RESTRICTION_DURATION() + 1);
-    }
+//         skip(veCVE.RESTRICTION_DURATION() + 1);
+//     }
 
-    function test_executeEpochFeeRouter_fail_whenChainIsNotSupported() public {
-        centralRegistry.addChainSupport(
-            address(protocolMessagingHub),
-            address(protocolMessagingHub),
-            address(cve),
-            _USDC_ADDRESS,
-            42161,
-            1,
-            1,
-            23
-        );
+//     function test_executeEpochFeeRouter_fail_whenChainIsNotSupported() public {
+//         centralRegistry.addChainSupport(
+//             address(protocolMessagingHub),
+//             address(protocolMessagingHub),
+//             address(cve),
+//             _USDC_ADDRESS,
+//             42161,
+//             1,
+//             1,
+//             23
+//         );
 
-        uint256 currentEpoch = cveLocker.currentEpoch(block.timestamp);
-        uint256 nextEpoch = cveLocker.nextEpochToDeliver();
+//         uint256 currentEpoch = cveLocker.currentEpoch(block.timestamp);
+//         uint256 nextEpoch = cveLocker.nextEpochToDeliver();
 
-        vm.prank(address(protocolMessagingHub));
+//         vm.prank(address(protocolMessagingHub));
 
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                FeeAccumulator.FeeAccumulator__CurrentEpochError.selector,
-                currentEpoch,
-                nextEpoch
-            )
-        );
-        feeAccumulator.executeEpochFeeRouter(42161, 0);
-    }
+//         vm.expectRevert(
+//             abi.encodeWithSelector(
+//                 FeeAccumulator.FeeAccumulator__CurrentEpochError.selector,
+//                 currentEpoch,
+//                 nextEpoch
+//             )
+//         );
+//         feeAccumulator.executeEpochFeeRouter(42161, 0);
+//     }
 
-    function test_executeEpochFeeRouter_success_whenChainIsNotSupported()
-        public
-    {
-        skip(veCVE.EPOCH_DURATION() * 2);
+//     function test_executeEpochFeeRouter_success_whenChainIsNotSupported()
+//         public
+//     {
+//         skip(veCVE.EPOCH_DURATION() * 2);
 
-        vm.expectRevert();
-        feeAccumulator.crossChainLockData(0);
+//         vm.expectRevert();
+//         feeAccumulator.crossChainLockData(0);
 
-        vm.prank(address(protocolMessagingHub));
-        feeAccumulator.executeEpochFeeRouter(42161, 0);
+//         vm.prank(address(protocolMessagingHub));
+//         feeAccumulator.executeEpochFeeRouter(42161, 0);
 
-        vm.expectRevert();
-        feeAccumulator.crossChainLockData(0);
-    }
+//         vm.expectRevert();
+//         feeAccumulator.crossChainLockData(0);
+//     }
 
-    function test_executeEpochFeeRouter_success_whenChainIsSupported_whenCVELockerIsNotShutdown()
-        public
-    {
-        _executeEpochFeeRouter();
+//     function test_executeEpochFeeRouter_success_whenChainIsSupported_whenCVELockerIsNotShutdown()
+//         public
+//     {
+//         _executeEpochFeeRouter();
 
-        assertEq(usdc.balanceOf(centralRegistry.daoAddress()), 0);
-        assertEq(usdc.balanceOf(address(cveLocker)), 6250000);
-    }
+//         assertEq(usdc.balanceOf(centralRegistry.daoAddress()), 0);
+//         assertEq(usdc.balanceOf(address(cveLocker)), 6250000);
+//     }
 
-    function test_executeEpochFeeRouter_success_whenChainIsSupported_whenCVELockerIsShutdown()
-        public
-    {
-        cveLocker.notifyLockerShutdown();
+//     function test_executeEpochFeeRouter_success_whenChainIsSupported_whenCVELockerIsShutdown()
+//         public
+//     {
+//         cveLocker.notifyLockerShutdown();
 
-        _executeEpochFeeRouter();
+//         _executeEpochFeeRouter();
 
-        assertEq(usdc.balanceOf(centralRegistry.daoAddress()), 6250000);
-        assertEq(usdc.balanceOf(address(cveLocker)), 0);
-    }
+//         assertEq(usdc.balanceOf(centralRegistry.daoAddress()), 6250000);
+//         assertEq(usdc.balanceOf(address(cveLocker)), 0);
+//     }
 
-    function _executeEpochFeeRouter() internal {
-        skip(3 weeks);
+//     function _executeEpochFeeRouter() internal {
+//         skip(3 weeks);
 
-        vm.prank(centralRegistry.feeAccumulator());
-        cveLocker.recordEpochRewards(_ONE);
+//         vm.prank(centralRegistry.feeAccumulator());
+//         cveLocker.recordEpochRewards(_ONE);
 
-        deal(address(protocolMessagingHub), _ONE);
-        deal(address(feeAccumulator), _ONE);
-        deal(_USDC_ADDRESS, address(feeAccumulator), 10e6);
-        deal(address(cve), address(this), _ONE);
+//         deal(address(protocolMessagingHub), _ONE);
+//         deal(address(feeAccumulator), _ONE);
+//         deal(_USDC_ADDRESS, address(feeAccumulator), 10e6);
+//         deal(address(cve), address(this), _ONE);
 
-        cve.approve(address(veCVE), _ONE);
+//         cve.approve(address(veCVE), _ONE);
 
-        veCVE.createLock(
-            _ONE,
-            true,
-            RewardsData(false, true, true, true),
-            "",
-            0
-        );
+//         veCVE.createLock(
+//             _ONE,
+//             true,
+//             RewardsData(false, true, true, true),
+//             "",
+//             0
+//         );
 
-        centralRegistry.addChainSupport(
-            address(protocolMessagingHub),
-            address(protocolMessagingHub),
-            address(cve),
-            _USDC_ADDRESS,
-            42161,
-            1,
-            1,
-            23
-        );
+//         centralRegistry.addChainSupport(
+//             address(protocolMessagingHub),
+//             address(protocolMessagingHub),
+//             address(cve),
+//             _USDC_ADDRESS,
+//             42161,
+//             1,
+//             1,
+//             23
+//         );
 
-        uint256 nextEpoch = cveLocker.nextEpochToDeliver();
+//         uint256 nextEpoch = cveLocker.nextEpochToDeliver();
 
-        vm.prank(address(protocolMessagingHub));
-        feeAccumulator.receiveCrossChainLockData(data);
+//         vm.prank(address(protocolMessagingHub));
+//         feeAccumulator.receiveCrossChainLockData(data);
 
-        (uint224 lockAmount, uint16 epoch, uint16 chainId) = feeAccumulator
-            .crossChainLockData(0);
+//         (uint224 lockAmount, uint16 epoch, uint16 chainId) = feeAccumulator
+//             .crossChainLockData(0);
 
-        assertEq(lockAmount, _ONE);
-        assertEq(chainId, 42161);
-        assertEq(epoch, nextEpoch);
+//         assertEq(lockAmount, _ONE);
+//         assertEq(chainId, 42161);
+//         assertEq(epoch, nextEpoch);
 
-        skip(veCVE.EPOCH_DURATION() * 2);
+//         skip(veCVE.EPOCH_DURATION() * 2);
 
-        vm.prank(address(protocolMessagingHub));
-        feeAccumulator.executeEpochFeeRouter(42161, 0);
+//         vm.prank(address(protocolMessagingHub));
+//         feeAccumulator.executeEpochFeeRouter(42161, 0);
 
-        vm.expectRevert();
-        feeAccumulator.crossChainLockData(0);
+//         vm.expectRevert();
+//         feeAccumulator.crossChainLockData(0);
 
-        assertEq(usdc.balanceOf(address(feeAccumulator)), 0);
-    }
-}
+//         assertEq(usdc.balanceOf(address(feeAccumulator)), 0);
+//     }
+// }
--- tests/architecture/FeeAccumulator/functions/ReceiveExecutableLockData.t.sol
+++ tests/architecture/FeeAccumulator/functions/ReceiveExecutableLockData.t.sol
@@ -1,26 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity 0.8.17;
 
-import { TestBaseFeeAccumulator } from "../TestBaseFeeAccumulator.sol";
-import { FeeAccumulator } from "contracts/architecture/FeeAccumulator.sol";
+// import { TestBaseFeeAccumulator } from "../TestBaseFeeAccumulator.sol";
+// import { FeeAccumulator } from "contracts/architecture/FeeAccumulator.sol";
 
-contract ReceiveExecutableLockDataTest is TestBaseFeeAccumulator {
-    function test_receiveExecutableLockData_fail_whenCallerIsNotMessagingHub()
-        public
-    {
-        vm.expectRevert(FeeAccumulator.FeeAccumulator__Unauthorized.selector);
-        feeAccumulator.receiveExecutableLockData(_ONE);
-    }
+// contract ReceiveExecutableLockDataTest is TestBaseFeeAccumulator {
+//     function test_receiveExecutableLockData_fail_whenCallerIsNotMessagingHub()
+//         public
+//     {
+//         vm.expectRevert(FeeAccumulator.FeeAccumulator__Unauthorized.selector);
+//         feeAccumulator.receiveExecutableLockData(_ONE);
+//     }
 
-    function test_receiveExecutableLockData_success() public {
-        uint256 nextEpochToDeliver = cveLocker.nextEpochToDeliver();
+//     function test_receiveExecutableLockData_success() public {
+//         uint256 nextEpochToDeliver = cveLocker.nextEpochToDeliver();
 
-        assertEq(cveLocker.epochRewardsPerCVE(nextEpochToDeliver), 0);
+//         assertEq(cveLocker.epochRewardsPerCVE(nextEpochToDeliver), 0);
 
-        vm.prank(address(protocolMessagingHub));
-        feeAccumulator.receiveExecutableLockData(_ONE);
+//         vm.prank(address(protocolMessagingHub));
+//         feeAccumulator.receiveExecutableLockData(_ONE);
 
-        assertEq(cveLocker.epochRewardsPerCVE(nextEpochToDeliver), _ONE);
-        assertEq(cveLocker.nextEpochToDeliver(), nextEpochToDeliver + 1);
-    }
-}
+//         assertEq(cveLocker.epochRewardsPerCVE(nextEpochToDeliver), _ONE);
+//         assertEq(cveLocker.nextEpochToDeliver(), nextEpochToDeliver + 1);
+//     }
+// }
