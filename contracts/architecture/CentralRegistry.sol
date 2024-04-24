a7270a7434bba39b1b8ad7176e4a0a85650ec1fc
Austinhs <austin_h_s@hotmail.com>
2024-04-23 23:16:58 -0400

    fixing some duped comments

--- contracts/architecture/CentralRegistry.sol
+++ contracts/architecture/CentralRegistry.sol
@@ -36,13 +36,10 @@ import { IMToken } from "contracts/interfaces/market/IMToken.sol";
 ///        operating team for any action, or the "Emergency Council" made up
 ///        of both Curvance Collective members and external stakeholders.
 ///
-///
 ///      All values inside Curvance are entered in basis point form. However,
-///      Fees are recorded internally in `WAD` format, or 1e18, rather than
 ///      Fees are recorded internally in `WAD` format, or 1e18, rather than
 ///      basis points, or 1e4. This is for greater precision in computations.
 ///      As a result, you will see multiplier values stored in 1e4 form,
-///      and fees stored in 1e18 form.
 ///      and fees stored in 1e18 form.
 ///
 ///      The Central Registry also manages the delegation system, creating
