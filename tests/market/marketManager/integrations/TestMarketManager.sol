15dc5c0ef436c2c342f690bd01ff483cefe7d3c3
zeuscurvance <zeus@curvance.com>
2024-05-03 06:11:18 -0400

    fix failing tests

--- tests/market/marketManager/integrations/TestMarketManager.sol
+++ tests/market/marketManager/integrations/TestMarketManager.sol
@@ -279,9 +279,10 @@ contract TestMarketManager is TestBaseMarketManagerEntropy {
         );
         assertEq(hasPosition, false);
 
-        vm.expectRevert(OracleRouter.OracleRouter__InvalidParameter.selector);
         (collateralSurplus, liquidityDeficit, positionsToClose) = marketManager
             .hypotheticalLiquidityOf(users[0], address(cTokens[0]), 0, 0);
+        assertEq(collateralSurplus, 0);
+        assertEq(liquidityDeficit, 0);
     }
 
     function testPositionCloseAfterRedeem() public {
@@ -343,9 +344,10 @@ contract TestMarketManager is TestBaseMarketManagerEntropy {
         );
         assertEq(hasPosition, false);
 
-        vm.expectRevert(OracleRouter.OracleRouter__InvalidParameter.selector);
         (collateralSurplus, liquidityDeficit, positionsToClose) = marketManager
             .hypotheticalLiquidityOf(users[0], address(cTokens[0]), 0, 0);
+        assertEq(collateralSurplus, 0);
+        assertEq(liquidityDeficit, 0);
     }
 
     function testPositionCloseAfterLiquidate() public {
