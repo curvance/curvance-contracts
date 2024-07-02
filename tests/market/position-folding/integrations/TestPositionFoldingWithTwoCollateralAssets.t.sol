13b68a5d9f514fe8489192f0a58fab02f69ee7e3
zeuscurvance <zeus@curvance.com>
2024-07-01 23:09:35 -0400

    fix compile issues

--- tests/market/position-folding/integrations/TestPositionFoldingWithTwoCollateralAssets.t.sol
+++ tests/market/position-folding/integrations/TestPositionFoldingWithTwoCollateralAssets.t.sol
@@ -404,15 +404,16 @@ contract TestPositionFoldingWithTwoCollateralAssets is TestBaseMarket {
         );
 
         uint256 amountForDeleverage = 0.3 ether;
-        deleverageData.swapData.inputToken = _WETH_ADDRESS;
-        deleverageData.swapData.inputAmount = amountForDeleverage;
-        deleverageData.swapData.outputToken = address(dai);
-        deleverageData.swapData.target = _UNISWAP_V2_ROUTER;
-        deleverageData.swapData.slippage = 50e16;
+        deleverageData.swapData = new SwapperLib.Swap[](1);
+        deleverageData.swapData[0].inputToken = _WETH_ADDRESS;
+        deleverageData.swapData[0].inputAmount = amountForDeleverage;
+        deleverageData.swapData[0].outputToken = address(dai);
+        deleverageData.swapData[0].target = _UNISWAP_V2_ROUTER;
+        deleverageData.swapData[0].slippage = 50e16;
         address[] memory path = new address[](2);
         path[0] = _WETH_ADDRESS;
         path[1] = address(dai);
-        deleverageData.swapData.call = abi.encodeWithSignature(
+        deleverageData.swapData[0].call = abi.encodeWithSignature(
             "swapExactTokensForTokens(uint256,uint256,address[],address,uint256)",
             amountForDeleverage,
             0,
