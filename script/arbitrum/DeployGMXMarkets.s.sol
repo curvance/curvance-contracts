aaa579a4ead2bb474af91d667145e2c53f0cbcd4
0xMai <chris@curvance.com>
2024-07-30 20:47:28 -0400

    Remove Curve oracle for asset pricing with sample since external integrators may get themselves in trouble with sampling logic

--- script/arbitrum/DeployGMXMarkets.s.sol
+++ script/arbitrum/DeployGMXMarkets.s.sol
@@ -1,37 +1,37 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.19;
 
-import { stdJson } from "forge-std/StdJson.sol";
-import { console } from "forge-std/console.sol";
-import { GMXMarketDeployer } from "../deployers/gmx/GMXMarketDeployer.s.sol";
+// import { stdJson } from "forge-std/StdJson.sol";
+// import { console } from "forge-std/console.sol";
+// import { GMXMarketDeployer } from "../deployers/gmx/GMXMarketDeployer.s.sol";
 
-contract DeployGMXMarkets is GMXMarketDeployer {
-    using stdJson for string;
+// contract DeployGMXMarkets is GMXMarketDeployer {
+//     using stdJson for string;
 
-    function run(string memory name) external {
-        _deploy("arbitrum", name);
-    }
+//     function run(string memory name) external {
+//         _deploy("arbitrum", name);
+//     }
 
-    function run(string memory network, string memory name) external {
-        _deploy(network, name);
-    }
+//     function run(string memory network, string memory name) external {
+//         _deploy(network, name);
+//     }
 
-    function _deploy(string memory network, string memory name) internal {
-        _setConfigurationPath(network);
-        _setDeploymentPath(network);
+//     function _deploy(string memory network, string memory name) internal {
+//         _setConfigurationPath(network);
+//         _setDeploymentPath(network);
 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
-        console.log("Deployer: ", deployer);
+//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+//         address deployer = vm.addr(deployerPrivateKey);
+//         console.log("Deployer: ", deployer);
 
-        vm.startBroadcast(deployerPrivateKey);
+//         vm.startBroadcast(deployerPrivateKey);
 
-        _deployGMXMarket(
-            name,
-            _readConfigAddress(string.concat(".gmx.", name)),
-            _readConfigAddress(string.concat(".gmx.", name, "AlteredToken"))
-        );
+//         _deployGMXMarket(
+//             name,
+//             _readConfigAddress(string.concat(".gmx.", name)),
+//             _readConfigAddress(string.concat(".gmx.", name, "AlteredToken"))
+//         );
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
