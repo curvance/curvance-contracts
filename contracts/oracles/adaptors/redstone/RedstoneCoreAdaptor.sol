d75bcc27a6b764930f1673bb5ea2de7764bb09a1
0xMai <chris@curvance.com>
2024-08-07 19:41:50 -0400

    Temporarily reduce MINIMUM_SIGNER_THRESHOLD_ALLOWED in redstone adaptor to 1 for redstone tests

--- contracts/oracles/adaptors/redstone/RedstoneCoreAdaptor.sol
+++ contracts/oracles/adaptors/redstone/RedstoneCoreAdaptor.sol
@@ -37,7 +37,8 @@ abstract contract RedstoneCoreAdaptor is BaseOracleAdaptor, PrimaryProdDataServi
     uint256 public constant DEFAULT_HEART_BEAT = 1 days;
     /// @notice The smallest value that Redstone Core unique signer threshold
     ///         can be inside Curvance.
-    uint256 public constant MINIMUM_SIGNER_THRESHOLD_ALLOWED = 2;
+    /// TODO: Update tests to use 2 minimum and change this back
+    uint256 public constant MINIMUM_SIGNER_THRESHOLD_ALLOWED = 1;
 
     /// STORAGE ///
 
