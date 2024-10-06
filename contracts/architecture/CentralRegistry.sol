6019248252854943c27a86d445a577dfd45b689b
0xMai <chris@curvance.com>
2024-10-06 18:20:38 -0400

    Added same elevated permissioning redundancy check for dao permissioning

--- contracts/architecture/CentralRegistry.sol
+++ contracts/architecture/CentralRegistry.sol
@@ -770,12 +770,16 @@ contract CentralRegistry is ERC165 {
         timelock = newTimelock;
 
         // Delete permission data.
-        delete hasDaoPermissions[previousTimelock];
-
         // If the previous Timelock also has Emergency Council permissions
         // for some reason, do not remove their elevated permissioning.
         if (previousTimelock != emergencyCouncil) {
             delete hasElevatedPermissions[previousTimelock];
+
+            // If the previous Timelock also has DAO permissions
+            // for some reason, do not remove their permissioning.
+            if (previousTimelock != daoAddress) {
+                delete hasDaoPermissions[previousTimelock];
+            }
         }
         
         // Add new permission data.
@@ -796,13 +800,16 @@ contract CentralRegistry is ERC165 {
         address previousEmergencyCouncil = emergencyCouncil;
         emergencyCouncil = newEmergencyCouncil;
 
-        // Delete permission data.
-        delete hasDaoPermissions[previousEmergencyCouncil];
-        
         // If the previous Emergency Council also has timelock permissions
         // for some reason, do not remove their elevated permissioning.
         if (previousEmergencyCouncil != timelock) {
             delete hasElevatedPermissions[previousEmergencyCouncil];
+
+            // If the previous Emergency Council also has DAO permissions
+            // for some reason, do not remove their permissioning.
+            if (previousEmergencyCouncil != daoAddress) {
+                delete hasDaoPermissions[previousEmergencyCouncil];
+            }
         }
         
         // Add new permission data.
