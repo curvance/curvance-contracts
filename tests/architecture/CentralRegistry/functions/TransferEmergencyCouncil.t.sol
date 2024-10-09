f59ebcfd0ad57c2a506ee9e53401e66378573c12
satherinja <Sather@Curvance.com>
2024-10-09 11:31:52 +0900

    fix/update tests with permission change

--- tests/architecture/CentralRegistry/functions/TransferEmergencyCouncil.t.sol
+++ tests/architecture/CentralRegistry/functions/TransferEmergencyCouncil.t.sol
@@ -5,7 +5,8 @@ import { TestBaseMarket } from "tests/market/TestBaseMarket.sol";
 import { CentralRegistry } from "contracts/architecture/CentralRegistry.sol";
 
 contract TransferEmergencyCouncilTest is TestBaseMarket {
-    address public newEmergencyCouncil = address(1000);
+    address public newEmergencyCouncil1 = address(1001);
+    address public newEmergencyCouncil2 = address(1002);
 
     event EmergencyCouncilTransferred(
         address indexed previousEmergencyCouncil,
@@ -18,20 +19,41 @@ contract TransferEmergencyCouncilTest is TestBaseMarket {
         vm.expectRevert(
             CentralRegistry.CentralRegistry__Unauthorized.selector
         );
-        centralRegistry.transferEmergencyCouncil(newEmergencyCouncil);
+        centralRegistry.transferEmergencyCouncil(newEmergencyCouncil1);
     }
 
     function test_transferEmergencyCouncil_success() public {
         assertTrue(centralRegistry.hasDaoPermissions(address(this)));
         assertTrue(centralRegistry.hasElevatedPermissions(address(this)));
+
         vm.expectEmit(true, true, true, true);
-        emit EmergencyCouncilTransferred(address(this), newEmergencyCouncil);
-        centralRegistry.transferEmergencyCouncil(newEmergencyCouncil);
-        assertTrue(centralRegistry.hasDaoPermissions(newEmergencyCouncil));
-        assertFalse(centralRegistry.hasDaoPermissions(address(this)));
+        emit EmergencyCouncilTransferred(address(this), newEmergencyCouncil1);
+
+        centralRegistry.transferEmergencyCouncil(newEmergencyCouncil1);
+
+        assertTrue(centralRegistry.hasDaoPermissions(newEmergencyCouncil1));
+        assertTrue(centralRegistry.hasDaoPermissions(address(this)));
         assertTrue(
-            centralRegistry.hasElevatedPermissions(newEmergencyCouncil)
+            centralRegistry.hasElevatedPermissions(newEmergencyCouncil1)
         );
-        assertFalse(centralRegistry.hasElevatedPermissions(address(this)));
+        assertTrue(centralRegistry.hasElevatedPermissions(address(this)));
+
+        vm.expectEmit(true, true, true, true);
+        emit EmergencyCouncilTransferred(
+            newEmergencyCouncil1,
+            newEmergencyCouncil2
+        );
+
+        vm.prank(newEmergencyCouncil1);
+        centralRegistry.transferEmergencyCouncil(newEmergencyCouncil2);
+
+        assertTrue(centralRegistry.hasDaoPermissions(newEmergencyCouncil2));
+        assertFalse(centralRegistry.hasDaoPermissions(newEmergencyCouncil1));
+        assertTrue(
+            centralRegistry.hasElevatedPermissions(newEmergencyCouncil2)
+        );
+        assertFalse(
+            centralRegistry.hasElevatedPermissions(newEmergencyCouncil1)
+        );
     }
 }
