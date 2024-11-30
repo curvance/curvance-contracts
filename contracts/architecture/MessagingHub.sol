0b9bceb12e8b24faccfc90fd471e8bceaaa8593d
0xMai <chris@curvance.com>
2024-11-30 12:27:14 -0500

    unwrap struct assumption for payloadType == 2 inside messaging  hub

--- contracts/architecture/MessagingHub.sol
+++ contracts/architecture/MessagingHub.sol
@@ -274,9 +274,15 @@ contract MessagingHub is QueryResponse {
         } else if (payloadType == 2) {
             // payloadType = 2: Crosschain Gauge Emission Configuration.
 
-            (, uint256 epoch, EmissionData memory emissionData) = abi.decode(
+            (
+                ,
+                uint256 epoch,
+                uint256 emissionTotal,
+                address[] memory tokens,
+                uint256[] memory emissions
+            ) = abi.decode(
                 payload,
-                (uint8, uint256, EmissionData)
+                (uint8, uint256, uint256, address[], uint256[])
             );
 
             IGaugeManager cachedGaugeManager = gaugeManager;
@@ -284,14 +290,14 @@ contract MessagingHub is QueryResponse {
             // Mint appropriate gauge emissions to Gauge Manager.
             cve.mintGaugeEmissions(
                 address(cachedGaugeManager),
-                emissionData.emissionTotal
+                emissionTotal
             );
 
             // Set upcoming epoch emissions for voted configuration.
             cachedGaugeManager.setEmissionRates(
                 epoch,
-                emissionData.tokens,
-                emissionData.emissions
+                tokens,
+                emissions
             );
         } else if (payloadType == 3) {
             // payloadType = 3:  Receiving fees from a foreign chain and
@@ -689,7 +695,6 @@ contract MessagingHub is QueryResponse {
     ) internal {
         // Query rewards for this epoch.
         uint256 feeTokensHeld = _getFeeTokenHeld();
-        
         // We temporary cache this chains lock points inside the currentChainId
         // variable since it will be overridden before it is ever called again.
         // We do this to avoid having to reserve another storage slot which will
