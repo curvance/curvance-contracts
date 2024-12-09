988090a22c2c170fba3ccf9fa91acc9ac815149f
satherinja <Sather@Curvance.com>
2024-12-09 21:35:49 +0900

    add more tests for liquidation validation

--- tests/market/marketManager/functions/LiquidateAccount.t.sol
+++ tests/market/marketManager/functions/LiquidateAccount.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import { TestBaseMarketManager } from "../TestBaseMarketManager.sol";
 import { MarketManager } from "contracts/market/MarketManager.sol";
+import { LiquidationManager } from "contracts/market/LiquidationManager.sol";
 
 contract LiquidateAccountTest is TestBaseMarketManager {
     function test_liquidateAccount_fail_whenCallerIsAccount() public {
@@ -16,6 +17,68 @@ contract LiquidateAccountTest is TestBaseMarketManager {
         marketManager.setSeizePaused(true);
 
         vm.expectRevert(MarketManager.MarketManager__Paused.selector);
+        marketManager.liquidateAccount(user1);
+    }
+
+    function test_liquidateAccount_fail_whenNoLiquidationAvailable() public {
+        vm.prank(user2);
+
+        vm.expectRevert(
+            MarketManager.MarketManager__NoLiquidationAvailable.selector
+        );
+        marketManager.liquidateAccount(user1);
+    }
+
+    function test_liquidateAccount_fail_whenNotEligibleForLiquidation()
+        public
+    {
+        _prepareLiquidation();
+
+        centralRegistry.setSequencingStatus(true);
+
+        vm.prank(user2, user2);
+
+        vm.expectRevert(
+            LiquidationManager.LiquidationManager__InvalidLiquidator.selector
+        );
+        marketManager.liquidateAccount(user1);
+    }
+
+    function test_liquidateAccount_fail_whenLiquidationWindowHasPassed()
+        public
+    {
+        _prepareLiquidation();
+
+        centralRegistry.setSequencingStatus(true);
+
+        vm.prank(user2);
+        marketManager.queueAccountLiquidation(user1);
+
+        skip(31);
+
+        vm.prank(user2, user2);
+
+        vm.expectRevert(
+            LiquidationManager.LiquidationManager__InvalidLiquidator.selector
+        );
+        marketManager.liquidateAccount(user1);
+    }
+
+    function test_liquidateAccount_fail_whenLiquidatorHasNoPriorityAccess()
+        public
+    {
+        _prepareLiquidation();
+
+        centralRegistry.setSequencingStatus(true);
+
+        vm.prank(user2);
+        marketManager.queueAccountLiquidation(user1);
+
+        vm.prank(user2, user2);
+
+        vm.expectRevert(
+            LiquidationManager.LiquidationManager__InvalidLiquidator.selector
+        );
         marketManager.liquidateAccount(user1);
     }
 }
