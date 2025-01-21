0f00b0cde9fc5ae7a02225a3efff72fed0ef427f
gas-limit <porporinoj2@montclair.edu>
2025-01-20 21:06:46 -0500

    revert changes to test/CompoundingWithExitFeePToken/functions/Redeem.t.sol

--- tests/market/token/CompoundingWithExitFeePToken/functions/Redeem.t.sol
+++ tests/market/token/CompoundingWithExitFeePToken/functions/Redeem.t.sol
@@ -4,10 +4,27 @@ pragma solidity ^0.8.19;
 import { TestBaseCompoundingWithExitFeePToken } from "../TestBaseCompoundingWithExitFeePToken.sol";
 import { CompoundingPToken } from "contracts/market/token/CompoundingPToken.sol";
 
-contract CompoundingWithExitFeePTokenPreviewWithdrawTest is
+contract CompoundingWithExitFeePTokenRedeemTest is
     TestBaseCompoundingWithExitFeePToken
 {
     event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function test_compoundingWithExitFeePTokenRedeem_fail_whenNoEnoughToRedeem()
+        public
+    {
+        vm.prank(address(1));
+        vm.expectRevert();
+        pBALRETHWithExitFee.redeem(100, address(this), address(this));
+    }
+    function test_compoundingWithExitFeePTokenRedeem_fail_whenAmountIsZero()
+        public
+    {
+        pBALRETHWithExitFee.mint(100, address(this));
+        vm.expectRevert(
+            CompoundingPToken.CompoundingPToken__ZeroAssets.selector
+        );
+        pBALRETHWithExitFee.redeem(0, address(this), address(this));
+    }
 
     function test_compoundingWithExitFeePTokenRedeem_success() public {
         pBALRETHWithExitFee.mint(100, address(this));
@@ -16,7 +33,14 @@ contract CompoundingWithExitFeePTokenPreviewWithdrawTest is
         uint256 balance = pBALRETHWithExitFee.balanceOf(address(this));
         uint256 totalSupply = pBALRETHWithExitFee.totalSupply();
 
-        uint256 quoteWithdraw = pBALRETHWithExitFee.previewRedeem(100);
+        vm.expectEmit(true, true, true, true, address(pBALRETHWithExitFee));
+        emit Transfer(address(this), address(0), 100);
+        pBALRETHWithExitFee.redeem(100, address(this), address(this));
 
+        assertEq(balRETH.balanceOf(address(this)), underlyingBalance + 98);
+        assertEq(pBALRETHWithExitFee.balanceOf(address(this)), balance - 100);
+        assertEq(pBALRETHWithExitFee.totalSupply(), totalSupply - 100);
     }
+
+
 }
