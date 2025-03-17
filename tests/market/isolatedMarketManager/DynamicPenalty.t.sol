26b60556b2078203bff19df446a684e2f0ded018
gas-limit <porporinoj2@montclair.edu>
2025-03-17 15:00:48 -0400

    Add isolated market support in TestBaseMarket and TestVariables, added tests for listTokens() and transient dynamic penalty
    1. Added mappings and variables to accommodate isolated versions of eUSDC and pBALRETH.
    2. Added deployment logic to include isolated dynamic interest rate models and tokens.

---
+++ tests/market/isolatedMarketManager/DynamicPenalty.t.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import { TestBaseMarketManager } from "tests/market/marketManager/TestBaseMarketManager.sol";
+
+contract DynamicPenaltyTest is TestBaseMarketManager {
+    address dappControlUser;
+
+    function setUp() public override {
+        super.setUp();
+        
+        // Setup market with tokens
+        deal(address(balRETH), address(this), 42069);
+        balRETH.approve(address(pBALRETHIsolated), 42069);
+
+        deal(address(_USDC_ADDRESS), address(this), 42069);
+        usdc.approve(address(eUSDCIsolated), 42069);
+        
+        // List tokens in the market
+        marketManagerIsolated.listTokens(address(pBALRETHIsolated), address(eUSDCIsolated));
+        
+        // Set position token parameters
+        marketManagerIsolated.updatePositionToken(
+            6600,    // collRatio 66%
+            5000,    // collReqSoft 50%
+            2500,    // collReqHard 25%
+            1000,    // liqIncBase 0%
+            500,     // liqIncMin 5%
+            2000,    // liqIncMax 20%
+            2000     // baseCFactor 20%
+        );
+        
+        // Create a dapp control user
+        dappControlUser = makeAddr("dappControlUser");
+        vm.startPrank(centralRegistry.daoAddress());
+        centralRegistry.addAuthorizedAtlasDAppControl(dappControlUser);
+        vm.stopPrank();
+    }
+
+    function testSetPenalty() public {
+        // Only dapp control can set penalty
+        vm.startPrank(dappControlUser);
+        
+        // Set a valid penalty (WAD + 15%)
+        uint256 validPenalty = 1.15e18;
+        marketManagerIsolated.setPenalty(validPenalty);
+        
+        // Verify the penalty was set correctly
+        assertEq(marketManagerIsolated.getLatestPenalty(), validPenalty);
+        
+        vm.stopPrank();
+    }
+    
+    function testSetPenaltyUnauthorized() public {
+        // Non-dapp control user should not be able to set penalty
+        vm.startPrank(user1);
+        
+        vm.expectRevert();
+        marketManagerIsolated.setPenalty(1.15e18);
+        
+        vm.stopPrank();
+    }
+    
+    function testSetPenaltyInvalidValue() public {
+        vm.startPrank(dappControlUser);
+        
+        // penalty below minimum
+        uint256 tooLowPenalty = 1.01e18;
+        // should revert with MarketManager__InvalidParameter()
+        vm.expectRevert(bytes4(0x65513fc1)); 
+        marketManagerIsolated.setPenalty(tooLowPenalty);
+        
+        // penalty above maximum
+        uint256 tooHighPenalty = 1.25e18; 
+        // should revert with MarketManager__InvalidParameter()
+        vm.expectRevert(bytes4(0x65513fc1)); 
+        marketManagerIsolated.setPenalty(tooHighPenalty);
+        
+        vm.stopPrank();
+    }
+    
+    function testResetPenalty() public {
+        vm.startPrank(dappControlUser);
+        
+        // Set a penalty
+        uint256 validPenalty = 1.15e18;
+        marketManagerIsolated.setPenalty(validPenalty);
+        assertEq(marketManagerIsolated.getLatestPenalty(), validPenalty);
+        
+        // Reset the penalty
+        marketManagerIsolated.resetPenalty();
+        
+        // Check that the penalty was reset to the default
+        uint256 defaultPenalty = 1.10e18; // 10% as set in setUp
+        assertEq(marketManagerIsolated.getLatestPenalty(), defaultPenalty);
+        
+        vm.stopPrank();
+    }
+    
+    function testResetPenaltyUnauthorized() public {
+        vm.startPrank(user1);
+        
+        vm.expectRevert();
+        marketManagerIsolated.resetPenalty();
+        
+        vm.stopPrank();
+    }
+    
+    function testGetLatestPenaltyDefault() public {
+        // should return the default
+        uint256 defaultPenalty = 1.10e18; // 10% as set in setUp
+        assertEq(marketManagerIsolated.getLatestPenalty(), defaultPenalty);
+    }
+    
+    function testPenaltyAffectsLiquidation() public {
+        // TODO: Implement this
+    }
+}
--- tests/utils/TestVariables.sol
+++ tests/utils/TestVariables.sol
@@ -17,6 +17,7 @@ import { EToken } from "contracts/market/token/EToken.sol";
 import { AuraPToken } from "contracts/market/token/AuraPToken.sol";
 import { DynamicInterestRateModel } from "contracts/market/DynamicInterestRateModel.sol";
 import { MarketManager } from "contracts/market/MarketManager.sol";
+import { MarketManagerIsolated } from "contracts/market/isolated/MarketManagerIsolated.sol";
 import { PendleZapper } from "contracts/plugins/market/PendleZapper.sol";
 import { VelodromeZapper } from "contracts/plugins/market/VelodromeZapper.sol";
 import { ChainlinkAdaptor } from "contracts/oracles/adaptors/chainlink/ChainlinkAdaptor.sol";
@@ -99,11 +100,14 @@ contract TestVariables {
     ChainlinkAdaptor public chainlinkAdaptor;
     ChainlinkAdaptor public dualChainlinkAdaptor;
     MarketManager public marketManager;
+    MarketManagerIsolated public marketManagerIsolated;
     OracleManager public oracleManager;
     EToken public eUSDC;
     EToken public eDAI;
+    EToken public eUSDCIsolated;
     AuraPToken public pBALRETH;
     MockAuraPTokenWithExitFee public pBALRETHWithExitFee;
+    AuraPToken public pBALRETHIsolated;
     IERC20 public usdc;
     IERC20 public dai;
     IERC20 public weth;
@@ -138,11 +142,14 @@ contract TestVariables {
     mapping(uint256 => ChainlinkAdaptor) public chainlinkAdaptors;
     mapping(uint256 => ChainlinkAdaptor) public dualChainlinkAdaptors;
     mapping(uint256 => MarketManager) public marketManagers;
+    mapping(uint256 => MarketManagerIsolated) public marketManagersIsolated;
     mapping(uint256 => OracleManager) public oracleManagers;
     mapping(uint256 => EToken) public eUSDCs;
     mapping(uint256 => EToken) public eDAIs;
+    mapping(uint256 => EToken) public eUSDCIsolateds;
     mapping(uint256 => AuraPToken) public pBALRETHs;
     mapping(uint256 => MockAuraPTokenWithExitFee) public pBALRETHWithExitFees;
+    mapping(uint256 => AuraPToken) public pBALRETHsIsolated;
 
     mapping(uint256 => MockV3Aggregator) public chainlinkUsdcUsds;
     mapping(uint256 => MockV3Aggregator) public chainlinkUsdcEths;
@@ -153,6 +160,8 @@ contract TestVariables {
 
     mapping(uint256 => mapping(address => DynamicInterestRateModel))
         public interestRateModels;
+
+    mapping(uint256 => mapping(address => DynamicInterestRateModel)) public isolatedInterestRateModels;
 
     mapping(uint256 => MockToken) public rewardTokens;
     mapping(uint256 => GaugeManager) public gaugeManagers;
@@ -368,11 +377,14 @@ contract TestVariables {
         chainlinkAdaptor = chainlinkAdaptors[chainId];
         dualChainlinkAdaptor = dualChainlinkAdaptors[chainId];
         marketManager = marketManagers[chainId];
+        marketManagerIsolated = marketManagersIsolated[chainId];
         oracleManager = oracleManagers[chainId];
         eUSDC = eUSDCs[chainId];
         eDAI = eDAIs[chainId];
+        eUSDCIsolated = eUSDCIsolateds[chainId];
         pBALRETH = pBALRETHs[chainId];
         pBALRETHWithExitFee = pBALRETHWithExitFees[chainId];
+        pBALRETHIsolated = pBALRETHsIsolated[chainId];
 
         chainlinkUsdcUsd = chainlinkUsdcUsds[chainId];
         chainlinkUsdcEth = chainlinkUsdcEths[chainId];
