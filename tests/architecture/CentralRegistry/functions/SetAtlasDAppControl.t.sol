15e4f4274b9a6f7e9a85cc7b36a288bf16f904d5
Amitabh Anand <amitabh009@gmail.com>
2025-04-23 18:07:07 +0530

    update SetAtlasDappControl.t.sol

--- tests/architecture/CentralRegistry/functions/SetAtlasDAppControl.t.sol
+++ tests/architecture/CentralRegistry/functions/SetAtlasDAppControl.t.sol
@@ -2,72 +2,65 @@
 pragma solidity ^0.8.19;
 
 import { TestBaseMarket } from "tests/market/TestBaseMarket.sol";
-import { CentralRegistry } from "contracts/architecture/CentralRegistry.sol";
-import { MarketManager } from "contracts/market/MarketManager.sol";
-import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
+import { MarketManagerIsolated } from "contracts/market/isolated/MarketManagerIsolated.sol";
 
-contract CentralRegistrySetAtlasDAppControlTest is TestBaseMarket {
-    // MarketManager[] internal _marketManagers;
+contract MarketManagerSetAtlasDAppControlTest is TestBaseMarket {
+    MarketManagerIsolated internal _marketManager;
 
-    // function setUp() public override {
-    //     super.setUp();
-    // }
+    function setUp() public override {
+        super.setUp();
+    }
 
-    // function test_centralRegistryAddAuthorizedAtlasDAppControl_fail_whenCallerIsNotAuthorized()
-    //     public
-    // {
-    //     vm.prank(address(1));
+    function test_marketManagerAddAuthorizedAtlasDAppControl_fail_whenCallerIsNotAuthorized()
+        public
+    {
+        vm.prank(address(1));
 
-    //     vm.expectRevert(CentralRegistry.CentralRegistry__Unauthorized.selector);
-    //     centralRegistry.addAuthorizedAtlasDAppControl(address(1));
-    // }
+        vm.expectRevert(MarketManagerIsolated.MarketManager__Unauthorized.selector);
+        marketManagerIsolated.addAuthorizedAtlasDAppControl(address(1));
+    }
 
-    // function test_centralRegistryAddAuthorizedAtlasDAppControl_success() public {
-    //     assertEq(centralRegistry.hasAtlasPermissions(address(1)), false);
+    function test_marketManagerAddAuthorizedAtlasDAppControl_success() public {
+        assertEq(marketManagerIsolated.hasAtlasPermissions(address(1)), false);
 
-    //     centralRegistry.addAuthorizedAtlasDAppControl(address(1));
+        marketManagerIsolated.addAuthorizedAtlasDAppControl(address(1));
 
-    //     assertEq(centralRegistry.hasAtlasPermissions(address(1)), true);
+        assertEq(marketManagerIsolated.hasAtlasPermissions(address(1)), true);
 
-    //     centralRegistry.addAuthorizedAtlasDAppControl(address(0));
+        marketManagerIsolated.addAuthorizedAtlasDAppControl(address(0));
 
-    //     assertEq(centralRegistry.hasAtlasPermissions(address(1)), true);
+        assertEq(marketManagerIsolated.hasAtlasPermissions(address(1)), true);
 
-    //     centralRegistry.removeAuthorizedAtlasDAppControl(address(1));
+        marketManagerIsolated.removeAuthorizedAtlasDAppControl(address(1));
 
-    //     assertEq(centralRegistry.hasAtlasPermissions(address(1)), false);
-    // }
+        assertEq(marketManagerIsolated.hasAtlasPermissions(address(1)), false);
+    }
 
-    // function test_centralRegistryRemoveAuthorizedAtlasDAppControl_fail_whenCallerIsNotAuthorized() public {
-    //     centralRegistry.addAuthorizedAtlasDAppControl(address(1));
-    //     vm.prank(address(2));
+    function test_marketManagerRemoveAuthorizedAtlasDAppControl_fail_whenCallerIsNotAuthorized() public {
+        marketManagerIsolated.addAuthorizedAtlasDAppControl(address(1));
+        vm.prank(address(2));
 
-    //     vm.expectRevert(CentralRegistry.CentralRegistry__Unauthorized.selector);
-    //     centralRegistry.unlockAtlasOev();
-    // }
+        vm.expectRevert(MarketManagerIsolated.MarketManager__Unauthorized.selector);
+        marketManagerIsolated.unlockAtlasCollateral(address(1));
+    }
 
-    // function test_centralRegistryLockAtlasOev_fail_whenCallerIsNotAuthorized() public {
-    //     centralRegistry.addAuthorizedAtlasDAppControl(address(1));
-    //     vm.prank(address(2));
+    function test_marketManagerLockAtlasCollateral_fail_whenCallerIsNotAuthorized() public {
+        marketManagerIsolated.addAuthorizedAtlasDAppControl(address(1));
+        vm.prank(address(2));
 
-    //     vm.expectRevert(CentralRegistry.CentralRegistry__Unauthorized.selector);
-    //     centralRegistry.lockAtlasOev();
-    // }
+        vm.expectRevert(MarketManagerIsolated.MarketManager__Unauthorized.selector);
+        marketManagerIsolated.lockAtlasCollateral();
+    }
 
-    // function test_centralRegistryLockUnlockAtlasOev_success() public {
-    //     centralRegistry.addAuthorizedAtlasDAppControl(address(5));
+    function test_marketManagerUnlockAtlasCollateral_success() public {
+        marketManagerIsolated.addAuthorizedAtlasDAppControl(address(5));
 
-    //     assertEq(centralRegistry.isAtlasOevAllowed(), false);
+        assertEq(marketManagerIsolated.hasAtlasPermissions(address(5)), true);
 
-    //     vm.prank(address(5));
-    //     centralRegistry.unlockAtlasOev();
+        vm.prank(address(5));
+        marketManagerIsolated.unlockAtlasCollateral(address(1));
 
-    //     assertEq(centralRegistry.isAtlasOevAllowed(), true);
-
-    //     vm.prank(address(5));
-    //     centralRegistry.lockAtlasOev();
-
-    //     assertEq(centralRegistry.isAtlasOevAllowed(), false);
-
-    // }
+        vm.prank(address(5));
+        marketManagerIsolated.lockAtlasCollateral();
+    }
 }
