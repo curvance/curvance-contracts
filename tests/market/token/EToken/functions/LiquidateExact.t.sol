e2cadb02f4b4718e4cb6e9a98e4c7b9d8ac53664
gas-limit <porporinoj2@montclair.edu>
2025-05-21 21:23:24 -0400

    Created TestBaseETokenIsolated, fixed liquidateExact on a single liquidation

--- tests/market/token/EToken/functions/LiquidateExact.t.sol
+++ tests/market/token/EToken/functions/LiquidateExact.t.sol
@@ -1,49 +1,50 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.19;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
-// import { TestBaseEToken } from "../TestBaseEToken.sol";
+import { TestBaseETokenIsolated } from "tests/market/token/EToken/TestBaseETokenIsolated.t.sol";
 
-// contract LiquidateExactTest is TestBaseEToken {
-//     function setUp() public override {
-//         super.setUp();
+contract LiquidateExactTest is TestBaseETokenIsolated {
+    function setUp() public override {
+        super.setUp();
 
-//         _prepareLiquidation();
-//     }
+        _prepareLiquidation();
+    }
 
-//     function test_liquidateExact_success() public {
-//         address[] memory accounts = new address[](1);
-//         accounts[0] = user1;
-//         uint256[] memory debtAmounts = new uint256[](1);
-//         debtAmounts[0] = 250e6;
+    // Test a single liquidation
+    function test_liquidateExact_success() public {
+        address[] memory accounts = new address[](1);
+        accounts[0] = user1;
+        uint256[] memory debtAmounts = new uint256[](1);
+        debtAmounts[0] = 250e6;
         
-//         vm.startPrank(user2);
-//         usdc.approve(address(eUSDC), 250e6);
-//         eUSDC.liquidateExact(
-//             accounts,
-//             debtAmounts,
-//             address(pBALRETH)
-//         );
-//         vm.stopPrank();
+        vm.startPrank(user2);
+        usdc.approve(address(eUSDC), 250e6);
+        eUSDC.liquidateExact(
+            accounts,
+            debtAmounts,
+            address(pBALRETH)
+        );
+        vm.stopPrank();
 
-//         _checkLiquidationResult();
-//     }
+        _checkLiquidationResult();
+    }
 
-//     function _checkLiquidationResult() internal {
-//         (uint256 balRETHPrice, ) = oracleManager.getPrice(
-//             address(balRETH),
-//             true,
-//             true
-//         );
+    function _checkLiquidationResult() internal {
+        (uint256 balRETHPrice, ) = oracleManager.getPrice(
+            address(balRETH),
+            true,
+            true
+        );
 
-//         assertApproxEqRel(
-//             pBALRETH.balanceOf(user1),
-//             _ONE - (500e18 * _ONE) / balRETHPrice,
-//             0.02e18
-//         );
-//         assertEq(pBALRETH.exchangeRateCached(), _ONE);
+        assertApproxEqRel(
+            pBALRETH.balanceOf(user1),
+            _ONE - (500e18 * _ONE) / balRETHPrice,
+            0.02e18
+        );
+        assertEq(pBALRETH.exchangeRateCached(), _ONE);
 
-//         assertEq(eUSDC.balanceOf(user1), 0);
-//         assertApproxEqRel(eUSDC.debtBalanceCached(user1), 750e6, 0.01e18);
-//         assertApproxEqRel(eUSDC.exchangeRateCached(), _ONE, 0.01e18);
-//     }
-// }
+        assertEq(eUSDC.balanceOf(user1), 0);
+        assertApproxEqRel(eUSDC.debtBalanceCached(user1), 750e6, 0.01e18);
+        assertApproxEqRel(eUSDC.exchangeRateCached(), _ONE, 0.01e18);
+    }
+}
