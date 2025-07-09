c8c1db5b44f107c218555e17eb67d1d1ed5d4b6b
gas-limit <joshsolidity@gmail.com>
2025-07-08 21:35:08 -0400

    add exchange rate assertion

--- tests/architecture/NativeUniversalBalance/integrations/TestNativeUniversalBalance.t.sol
+++ tests/architecture/NativeUniversalBalance/integrations/TestNativeUniversalBalance.t.sol
@@ -796,8 +796,10 @@ contract TestNativeUniversalBalance is TestBaseMarketIsolated {
         skip(10 weeks);
 
         vm.prank(user1);
-        console2.log("=== START LOG CHANGE HERE ===");
+        console2.log("=== START LOG INSPECTION HERE ===");
         nativeUniversalBalance.withdrawNative(50e18, true, address(this));
+
+        assertGt(eWETH.exchangeRateUpdated(), 1e18);
 
         (uint256 sittingBalance, uint256 lentBalance) = nativeUniversalBalance
             .userBalances(user1);
