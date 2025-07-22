e09a7c85ed7ed72c01b06812bff0e67a7383e28d
0xMai <chris@curvance.com>
2025-07-22 00:16:39 -0400

    Add unlockAuctionCollateral test suite

---
+++ tests/market/MarketManagerIsolated/functions/UnlockAuctionCollateral.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import { MarketManagerIsolated } from "contracts/market/isolated/MarketManagerIsolated.sol";
+
+import { TestBaseMarketIsolated } from "tests/market/TestBaseMarketIsolated.sol";
+
+contract UnlockAuctionCollateralTest is TestBaseMarketIsolated {
+
+    function setUp() public override {
+        super.setUp();
+
+        deal(address(balRETH), address(this), 77777);
+        balRETH.approve(address(strategyCBALRETH), 77777);
+
+        deal(address(_USDC_ADDRESS), address(this), 77777);
+        usdc.approve(address(borrowableCUSDC), 77777);
+        
+        // List tokens in the market.
+        marketManagerIsolated.listTokens(address(strategyCBALRETH), address(borrowableCUSDC));
+
+        _setCTokenConfigBasic(address(strategyCBALRETH), 100_000e18, 0);
+        _setCTokenConfigBasic(address(borrowableCUSDC), 0, 1_000_000e6);
+    }
+
+    function test_unlockAuctionCollateral_fail_whenUnauthorized() public {
+        // // Non-dapp control user should not be able to set penalty
+        vm.startPrank(user1);
+        
+        vm.expectRevert(MarketManagerIsolated.MarketManager__Unauthorized.selector);
+        marketManagerIsolated.unlockAuctionCollateral(address(strategyCBALRETH), 1.15e18, 0.30e18);
+        
+        vm.stopPrank();
+    }
+
+    function test_unlockAuctionCollateral_success() public {
+        vm.startPrank(dappControlUser);
+
+        marketManagerIsolated.unlockAuctionCollateral(address(strategyCBALRETH));
+        vm.stopPrank();
+    }
+
+
+}
