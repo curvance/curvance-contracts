c7af001d62ad5c3d608a4000a6c752923701e3be
Austin-s <austin@DESKTOP-TE8F6U3.localdomain>
2025-07-27 19:45:43 -0400

    Add script to handle individual Mock Oracle addons for tokens

---
+++ script/deployment/AddMockOracleSupport.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { Script } from "forge-std/Script.sol";
+import { Vm } from "forge-std/Vm.sol";
+import { DeploymentLogger } from "../utils/DeploymentLogger.sol";
+import { MockOracleAdaptor } from "contracts/mocks/MockOracleAdaptor.sol";
+import { OracleManager } from "contracts/oracles/OracleManager.sol";
+import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
+
+contract AddMockOracleSupport is Script {
+    event ContractDeployed(address contractAddress, string contractName);
+
+    DeploymentLogger logger;
+
+    function run(
+        address registry,
+        address mockOracle,
+        address[] calldata assets,
+        uint240[] calldata usdPrices,
+        uint240[] calldata nativePrices
+    ) external {
+        logger = new DeploymentLogger();
+        vm.recordLogs();
+        vm.startBroadcast();
+
+        ICentralRegistry cr = ICentralRegistry(registry);
+        OracleManager oracleManager = OracleManager(cr.oracleManager());
+        MockOracleAdaptor adaptor = MockOracleAdaptor(mockOracle);
+
+        for (uint256 i = 0; i < assets.length; i++) {
+            address asset = assets[i];
+            uint240 usdPrice = usdPrices[i];
+            uint240 nativePrice = nativePrices[i];
+
+            adaptor.addAsset(asset);
+            adaptor.setPrice(asset, usdPrice, nativePrice);
+            oracleManager.addAssetPriceFeed(asset, address(adaptor));
+        }
+
+        vm.stopBroadcast();
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        logger.saveLogsToDeployment(logs);
+    }
+}
