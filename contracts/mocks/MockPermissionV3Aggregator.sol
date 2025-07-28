965348079512f4cbcf7cb3ec31df5ee05cd301ff
Austin-s <austin@DESKTOP-TE8F6U3.localdomain>
2025-07-28 00:47:44 -0400

    Create MockPermissionV3Aggregator

---
+++ contracts/mocks/MockPermissionV3Aggregator.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { MockV3Aggregator } from "contracts/mocks/MockV3Aggregator.sol";
+import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
+
+/**
+ * @title MockPermissionV3Aggregator
+ * @notice A mock aggregator that allows elevated permissions to update answers
+ *         Mainly used for testnet.
+ */
+contract MockPermissionV3Aggregator is MockV3Aggregator {
+    ICentralRegistry public registry;
+
+    constructor(
+        ICentralRegistry _centralRegistry,
+        uint8 _decimals,
+        int256 _initialAnswer,
+        int192 _maxAnswer,
+        int192 _minAnswer
+    ) MockV3Aggregator(_decimals, _initialAnswer, _maxAnswer, _minAnswer) {
+        registry = _centralRegistry;
+    }
+
+    function updateAnswer(int256 _answer) public override {
+        if (!registry.hasElevatedPermissions(msg.sender)) {
+            revert("MockPermissionV3Aggregator: Unauthorized");
+        }
+        super.updateAnswer(_answer);
+    }
+
+    function updateRoundData(
+        uint80 _roundId,
+        int256 _answer,
+        uint256 _timestamp,
+        uint256 _startedAt
+    ) public override {
+        if (!registry.hasElevatedPermissions(msg.sender)) {
+            revert("MockPermissionV3Aggregator: Unauthorized");
+        }
+        super.updateRoundData(_roundId, _answer, _timestamp, _startedAt);
+    }
+}
