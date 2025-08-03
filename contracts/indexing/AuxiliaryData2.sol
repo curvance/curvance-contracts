4bb5f0725c09b1a3430d642e2d5bb63a85d683e2
Austin <austin_h_s@hotmail.com>
2025-08-03 16:25:13 -0400

    update from laptop

--- contracts/indexing/AuxiliaryData2.sol
+++ contracts/indexing/AuxiliaryData2.sol
@@ -2,18 +2,90 @@
 pragma solidity ^0.8.26;
 
 import { MarketManagerIsolated } from "contracts/market/isolated/MarketManagerIsolated.sol";
+import { LiquidityManagerIsolated } from "contracts/market/isolated/LiquidityManagerIsolated.sol";
 import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
 import { IVeCVE } from "contracts/interfaces/IVeCVE.sol";
 
 contract AuxiliaryData2 {
     ICentralRegistry public immutable centralRegistry;
 
+    struct StaticMarketAsset {
+        address _address;
+        string name;
+        string symbol;
+        uint8 decimals;
+        uint256 totalSupply;
+    }
+
+    struct StaticMarketToken {
+        address _address;
+        StaticMarketAsset asset;
+        uint256 collateralCap;
+        LiquidityManagerIsolated.CurvanceToken config;
+        uint256[2] adapters;
+        uint256 totalSupply; // totalSupply - reserved
+    }
+
+    struct DynamicMarketToken {
+        address _address;
+        uint256 posted;
+        uint256 sharePrice;
+        uint256 tokenPrice;
+        uint256 tvl;
+        uint256 borrowRate;
+        uint256 utilizationRate;
+        uint256 supplyRate;
+        uint256 predicted_supplyRate;
+        uint256 liquidity;
+    }
+
+    struct DynamicMarketData {
+        address _address;
+        DynamicMarketToken[] tokens;
+    }
+
+    struct StaticMarketData {
+        address _address;
+        StaticMarketToken[] tokens;
+    }
+
+    struct UserMarketToken {
+        address _address;
+        bool hasPosition;
+        uint256 tokenAmount;
+        uint256 shareAmount;
+        uint256 debt;
+    }
+
     struct UserData {
-        uint256[] userLocks;
+        uint256[] locks;
+        uint256[] cooldowns;
+        mapping(address => UserMarketToken[]) marketTokens;
     }
 
     constructor(ICentralRegistry centralRegistry_) {
         centralRegistry = centralRegistry_;
+    }
+
+    function getAllDynamicState() public view {}
+
+    function getStaticMarketData() public view {
+        // address[] memory markets = centralRegistry.marketManagers();
+    }
+
+    function getDynamicMarketData() public view {
+        // address[] memory markets = centralRegistry.marketManagers();
+    }
+
+    function getUserData(
+        address account
+    ) public view returns (UserData memory data) {
+        // Load locks
+        (data.locks, ) = IVeCVE(centralRegistry.veCVE()).queryUserLocks(
+            account
+        );
+
+        return data;
     }
 
     function marketMultiCooldown(
@@ -28,22 +100,5 @@ contract AuxiliaryData2 {
             cooldowns[i] = cooldownTimestamp + mm.MIN_HOLD_PERIOD();
         }
         return cooldowns;
-    }
-
-    function getAllDynamicSate() public view {}
-
-    function getStaticMarketData() public view {}
-
-    function getDynamicMarketData() public view {}
-
-    function getUserData(
-        address account
-    ) public view returns (UserData memory data) {
-        // Load locks
-        (data.userLocks, ) = IVeCVE(centralRegistry.veCVE()).queryUserLocks(
-            account
-        );
-
-        return data;
     }
 }
