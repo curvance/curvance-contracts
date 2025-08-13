ef4d82fb37c526ada5fb3fb1e714fa7fc7dc1cbf
0xMai <chris@curvance.com>
2025-08-12 23:14:30 -0400

    nit: fix PythAdaptorMulticall that still was inputting old config params to addAsset

--- tests/market/multicall/TestPythAdaptorMulticall.t.sol
+++ tests/market/multicall/TestPythAdaptorMulticall.t.sol
@@ -63,12 +63,12 @@ contract TestPythAdaptorMulticall is TestBaseMarketIsolated {
             _WETH_ADDRESS
         );
 
-        PythAdaptor.AssetConfig memory data;
-        data.isConfigured = true;
-        data.heartbeat = 1 days;
-        data.priceId =
-            0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43;
-        adaptor.addAsset(_WBTC_ADDRESS, true, data);
+        adaptor.addAsset(
+            _WBTC_ADDRESS,
+            true,
+            1 days,
+            0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43
+        );
         vm.warp(1711335100);
 
         multicallChecker = new PythAdaptorMulticallChecker(ICentralRegistry(address(centralRegistry)));
