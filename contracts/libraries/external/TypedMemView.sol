a2957bcd6911f06aa32db308a0d7bd513792fa1d
0xMai <chris@curvance.com>
2025-08-16 12:06:58 -0400

    Fix some accidently mislicensed contracts

--- contracts/libraries/external/TypedMemView.sol
+++ contracts/libraries/external/TypedMemView.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1 OR Apache-2.0
+// SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
