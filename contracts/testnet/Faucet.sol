8be89a61ca0aa0f0b42c8c0a1075255d620c8d87
Austinhs <austin_h_s@hotmail.com>
2025-08-21 03:53:03 -0400

    disable gas-custom-errors for linter in Faucet contract

--- contracts/testnet/Faucet.sol
+++ contracts/testnet/Faucet.sol
@@ -7,6 +7,7 @@ import { SafeTransferLib } from "contracts/libraries/external/SafeTransferLib.so
 import { IERC20 } from "contracts/interfaces/IERC20.sol";
 // solhint-enable max-line-length
 
+// solhint-disable gas-custom-errors
 contract Faucet is Ownable {
     struct FaucetToken {
         address tokenAddress;
