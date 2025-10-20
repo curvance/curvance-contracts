f34925c3e6facc373f338f11f7899db69bd5dbb6
Austinhs <austin_h_s@hotmail.com>
2025-10-20 10:55:13 -0400

    Add deployer for calldata checkers -- with support for kuru

---
+++ script/deployment/DeployCalldataChecker.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import { DeployScript } from "../utils/DeployScript.sol";
+
+import { KuruCalldataChecker } from "contracts/calldata-checker/swap-checker/KuruCalldataChecker.sol";
+import { CentralRegistry } from "contracts/architecture/CentralRegistry.sol";
+
+contract DeployCalldataChecker is DeployScript {
+    struct AvailableCheckers {
+        address kuruRouter;
+    }
+
+    function run(address registry, AvailableCheckers calldata checkerSelection) external recordEvents {
+        CentralRegistry cr = CentralRegistry(registry);
+        
+        if(checkerSelection.kuruRouter != address(0)) {
+            KuruCalldataChecker checker = new KuruCalldataChecker(checkerSelection.kuruRouter);
+            cr.setExternalCalldataChecker(checkerSelection.kuruRouter, address(checker));
+        }
+    }
+}
