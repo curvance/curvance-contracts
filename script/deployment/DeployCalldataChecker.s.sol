1889b46750be0fad1adcde0324c1a2c1b18ceba1
Austinhs <austin_h_s@hotmail.com>
2025-11-11 14:34:06 -0500

    Update fee collector for kuru

--- script/deployment/DeployCalldataChecker.s.sol
+++ script/deployment/DeployCalldataChecker.s.sol
@@ -15,7 +15,7 @@ contract DeployCalldataChecker is DeployScript {
         CentralRegistry cr = CentralRegistry(registry);
         
         if(checkerSelection.kuruRouter != address(0)) {
-            KuruCalldataChecker checker = new KuruCalldataChecker(checkerSelection.kuruRouter, 0xc45F0aDD4981076928537490F8C0e24944288947, cr.daoAddress());
+            KuruCalldataChecker checker = new KuruCalldataChecker(checkerSelection.kuruRouter, 0xe661C9435ad0365E9274df9C1D142fbA004E5Ad1, cr.daoAddress());
             cr.setExternalCalldataChecker(checkerSelection.kuruRouter, address(checker));
         }
     }
