1795788c7ec421eb047fe9f4485ff0685ab9220b
gas-limit <joshsolidity@gmail.com>
2026-05-27 10:56:49 -0400

    update DeployOptimizerReader run parameter

--- script/deployment/DeployOptimizerReader.s.sol
+++ script/deployment/DeployOptimizerReader.s.sol
@@ -6,11 +6,11 @@ import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
 import { OptimizerReader } from "contracts/views/OptimizerReader.sol";
 
 contract DeployOptimizerReader is DeployScript {
-    address centralRegistryAddress = 0x1310f352f1389969Ece6741671c4B919523912fF;
     uint256 stalenessMultiplier = 11000;
 
 
     function run(
+        address centralRegistryAddress
     ) external recordEvents {
         OptimizerReader reader = new OptimizerReader(
             ICentralRegistry(centralRegistryAddress),
