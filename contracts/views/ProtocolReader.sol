41d97c3fb1935576f821afbe19d0aa83130e57b7
gas-limit <joshsolidity@gmail.com>
2026-06-29 13:36:14 -0400

    redeemPaused in ProtocolReader

--- contracts/views/ProtocolReader.sol
+++ contracts/views/ProtocolReader.sol
@@ -20,6 +20,10 @@ import { IERC20 } from "contracts/interfaces/IERC20.sol";
 import { IDynamicIRM } from "contracts/interfaces/IDynamicIRM.sol";
 import { DynamicIRM } from "contracts/market/DynamicIRM.sol";
 
+interface IMarketRedeemPause {
+    function redeemPaused() external view returns (uint8);
+}
+
 contract ProtocolReader {
     /// TYPES ///
 
@@ -42,6 +46,7 @@ contract ProtocolReader {
         bool mintPaused;
         bool collateralizationPaused;
         bool borrowPaused;
+        bool redeemPaused;
         bool isBorrowable;
         uint256 collRatio;
         uint256 maxLeverage;
@@ -230,6 +235,8 @@ contract ProtocolReader {
             StaticMarketToken[] memory tokens = new StaticMarketToken[](
                 numTokens
             );
+            bool redeemPaused =
+                IMarketRedeemPause(address(mm)).redeemPaused() == 2;
 
             uint256[] memory uniqueAdapters;
             for (uint256 j; j < numTokens; ++j) {
@@ -242,7 +249,11 @@ contract ProtocolReader {
                 uniqueAdapters = _addUniqueAdapter(uniqueAdapters, oracleA);
                 uniqueAdapters = _addUniqueAdapter(uniqueAdapters, oracleB);
 
-                tokens[j] = _getStaticTokenConfig(mm, cToken);
+                tokens[j] = _getStaticTokenConfig(
+                    mm,
+                    cToken,
+                    redeemPaused
+                );
                 tokens[j].adapters = [oracleA, oracleB];
             }
 
@@ -1197,11 +1208,13 @@ contract ProtocolReader {
     /// @param mm The market manager to pull static token data from.
     /// @param cToken The address of the cToken to pull static token
     ///               configuration of.
+    /// @param redeemPaused Whether market-wide redemptions are paused.
     /// @return t A StaticMarketToken struct containing static token
     ///           configuration information.
     function _getStaticTokenConfig(
         IMarketManager mm,
-        ICToken cToken
+        ICToken cToken,
+        bool redeemPaused
     ) internal view returns (StaticMarketToken memory t) {
         t._address = address(cToken);
         t.name = cToken.name();
@@ -1215,6 +1228,7 @@ contract ProtocolReader {
         t.isListed = _isListed(mm, address(cToken));
         (t.mintPaused, t.collateralizationPaused, t.borrowPaused) =
             mm.actionsPaused(address(cToken));
+        t.redeemPaused = redeemPaused;
         t.isBorrowable = cToken.isBorrowable();
         (t.collRatio, t.collReqSoft, t.collReqHard) = _collConfig(mm, address(cToken));
         t.maxLeverage = _mulDiv(BPS, BPS, BPS - t.collRatio);
